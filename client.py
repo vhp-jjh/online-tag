50077564e667332561feef68f126b9fc3bc5f41a
vpong <vitchyr@gmail.com>
2014-02-01 11:46:30 -0500

    Server and client now send random data back and forth over the internet.

--- client.py
+++ client.py
@@ -1,9 +1,39 @@
 import socket
+import pickle
+from random import randrange
 
-s = socket.socket()
-host = socket.gethostname() # Local machine name
-port = 12122
+class Client:
+  def __init__(self, host, port):
+    self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.s.connect((host, port))
 
-s.connect((host, port))
-print(s.recv(1024).decode('UTF-8'))
-s.close()
+  def wait_to_start(self):
+    data = self.s.recv(1024)
+    while len(data) == 0:
+      data = self.s.recv(1024)
+
+  def send_vel(self, vel):
+    data = pickle.dumps(vel) # Assuming vel is an int tuple (x, y)
+    self.s.sendall(data)
+
+  def close_conn(self):
+    self.s.close()
+
+  def read_game_data(self): 
+    data = self.s.recv(1024)
+    msg = pickle.loads(data)
+    return msg
+
+if __name__ == "__main__":
+  host = '10.32.153.218' # Vitchyr's Gateway
+  port = 12121
+  client = Client(host, port)
+
+  client.wait_to_start()
+  while True:
+    client.send_vel((randrange(10), randrange(10)))
+
+    data = client.read_game_data()
+    print(data)
+  
+  #client.close_conn()
--- server.py
+++ server.py
@@ -1,16 +1,63 @@
 import socket
+import pickle
+from random import randrange
 
-s = socket.socket()
-host = socket.gethostname() # Local machine name
-port = 12122
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # forcefully take
-                                                        # the address
-s.bind((host, port))
+class Server:
+  def __init__(self, _host, _port):
+    self.s = socket.socket(socket.AF_INET)
+    self.s.bind((host, port))
+    self.conns = []
 
-s.listen(5)
-while True:
-  c, addr = s.accept()      # Estabilish connection with client
-  print("Got connection from", addr)
-  msg = "Thank you for connecting"
-  c.send(msg.encode('UTF-8'))
-  c.close()
+  def listen_for_conns(self, n_players):
+    for n_joined in range(n_players):
+      print("Waiting for %d players to connect..." % (n_players - n_joined))
+      self.s.listen(5)
+      conn, addr = self.s.accept()      # Estabilish connection with client
+      print("Got connection from", addr)
+      self.conns.append((conn, addr))
+  
+  def start_game(self):
+    for conn, addr in self.conns:
+      conn.send(b'\x01')
+
+  def read_vels(self):
+    vels = {}
+    for conn, addr in self.conns:
+      # Test reading and sending tuples
+      data = conn.recv(1024)
+      if len(data) > 0:
+        vels[addr] = pickle.loads(data)
+      else:
+        vels[addr] = (0,0)
+    return vels
+
+  def send_game_data(self, game_data):
+    data = pickle.dumps(game_data)
+    for conn, addr in self.conns:
+      conn.send(data)
+
+  def close_conns(self):
+    for conn, addr in self.conns:
+      conn.close()
+    self.conns = []
+
+if __name__ == "__main__":
+  host = '10.32.153.218' # Vitchyr's Gateway
+  port = 12121
+  server = Server(host, port)
+  
+  num_players = 2
+  server.listen_for_conns(2)
+  server.start_game()
+
+  while True:
+    vels = server.read_vels()
+    print("Received velocities", vels)
+
+    # calculate message
+    game_data= [(randrange(10), randrange(10)), (randrange(10),randrange(10))]
+    #game_data = engine.get_game_data()
+
+    server.send_game_data(game_data)
+
+    #server.close_conns()
